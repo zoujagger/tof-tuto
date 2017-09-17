a593f480ab991d065f5e03fc90f82bbb06f0f7d9
zoujagger <ulrichjagger@gmail.com>
2017-09-17 19:53:46 +0200

    add master -details features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,17 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
+
+  // expose heroes
+  heroes = HEROES;
+
+  // selected hero
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
 }
 
 // declare an hero class
@@ -22,3 +33,17 @@ export class Hero {
   id: number;
   name: string;
 }
+
+// an array of Type Hero
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
