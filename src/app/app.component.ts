72b819c405ee151f0e7381494dba52c743764815
zoujagger <ulrichjagger@gmail.com>
2017-09-17 21:34:54 +0200

    add services with promise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,48 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <app-heroes></app-heroes>
+  `
 })
-
 export class AppComponent {
-
   title = 'Tour of Heroes';
-  // hero = 'winstorm' its a string var
-  // initialise an objet of class hero
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  // expose heroes
-  heroes = HEROES;
-
-  // selected hero
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
 }
-
-// declare an hero class
-export class Hero {
-  id: number;
-  name: string;
-}
-
-// an array of Type Hero
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 
 // add tuto module step by step
 // NgModel lives here them import in imports part
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,   // Add HeroDetailComponent
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ HeroService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
